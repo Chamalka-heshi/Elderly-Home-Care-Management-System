7465234eaffd489d827cd7b1e22225bf6c02ba19
chathunijanadhi <janadhichathuni@gmail.com>
2026-02-21 17:09:29 +0530

    Redesign homepage styles

--- frontend/src/features/home/Home.tsx
+++ frontend/src/features/home/Home.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./homePage.css";
-import "./indexEffects.css";
+
 
 import Navbar from "../../components/Navbar/navbar";
 import Footer from "../../components/Footer/footer";
