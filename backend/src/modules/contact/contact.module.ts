a3d9d106e47dd0804be7e75d836eb8f94fd1f6cc
Poorna-danushka <poornadanushka2@gmail.com>
2026-03-30 18:34:23 +0530

    Add ContactInfo entity in ContactModule

--- backend/src/modules/contact/contact.module.ts
+++ backend/src/modules/contact/contact.module.ts
@@ -1,11 +1,13 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ContactMessage } from './entities/contact-message.entity';
-import { ContactService } from './contact.service';
+import { ContactInfo }    from './entities/contact-info.entity';
+import { ContactService }    from './contact.service';
 import { ContactController } from './contact.controller';
 
 @Module({
-  imports:     [TypeOrmModule.forFeature([ContactMessage])],
+  imports:     [TypeOrmModule.forFeature([ContactMessage, ContactInfo])],
   controllers: [ContactController],
   providers:   [ContactService],
   exports:     [ContactService],
