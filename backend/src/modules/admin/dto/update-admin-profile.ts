18ec526377974aa283a813160b036291e7392d68
Poorna-danushka <poornadanushka2@gmail.com>
2026-04-08 12:16:14 +0530

    Add admin profile update DTO

---
+++ backend/src/modules/admin/dto/update-admin-profile.ts
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import { IsString, IsOptional, Matches } from 'class-validator';
+
+export class UpdateUserDto {
+  @IsOptional()
+  @IsString()
+  fullName?: string;
+
+  @IsOptional()
+  @IsString()
+  @Matches(/^[0-9]{10}$/, { message: 'Contact number must be 10 digits' })
+  contactNumber?: string;
+}
